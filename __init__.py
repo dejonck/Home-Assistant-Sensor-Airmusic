9d03d0e2a30f1c619520ebb0e6a5b0cb9bb0c8ec
dejonck <github@dejonck.be>
2023-01-18 14:58:17 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-import logging
-from homeassistant.const import EVENT_HOMEASSISTANT_START, EVENT_HOMEASSISTANT_STOP
-import homeassistant.helpers.config_validation as cv
-import voluptuous as vol
-import threading
-from . import rcswitch
